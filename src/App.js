75f617a242c45429f1efe3b43a84979b11e230be
Mateo Paez <paezball@ualberta.ca>
2024-08-28 13:08:40 -0600

    Changed back to BrowserRouter and added a 404 handling file to try and fix 404 error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from "./components/Nav.js";
 import Background from './components/Background.js';
 import About from "./components/About.js";
