e0e406eafb1b80608659ba549e7f5bdfaaf04437
Mateo Paez <paezball@ualberta.ca>
2024-09-02 04:37:38 -0600

    fixing resume.pdf link for pages

--- src/components/Resume/ResumeBtn.js
+++ src/components/Resume/ResumeBtn.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import resume from '../../assets/MateoPaez-Resume.pdf';
-
 
 export default function ResumeBtn() {
     const handleButtonClick = () => {
-        window.open(resume);  // opens in a new tab
+        window.open("/MateoPaez-Resume.pdf");  // opens in a new tab
     };
 
     return (
