cf3d1aaee214ec61ededf6b749192fc725ffa682
Mateo Paez <paezball@ualberta.ca>
2024-11-23 19:55:46 -0700

    changed projectOne to Voxa

--- src/components/Projects/ProjectsData.js
+++ src/components/Projects/ProjectsData.js
@@ -1,35 +1,35 @@
-import projectOne from "../../assets/gobbers.png";
-import projectTwo from "../../assets/habitup.png";
+import projectOne from "../../assets/voxa.png";
+import projectTwo from "../../assets/gobbers.png";
 import projectThree from "../../assets/404project.png";
 
 const projects = {
   1: {
-    title: "Gobber's Hoard",
+    title: "Voxa",
     image: projectOne,
     description: (
       <>
         <p>
-          Small looter/stealth video game developed in RPG Maker MV, built using JavaScript. Member of a 5-person team where my primary roles where
-          level design and building, implementing mechanics using custom made plugins, and general quality assurance and bug testing.
+        Voxa is a voice-activated AI assistant designed to provide seamless, fully voice-to-voice interaction. 
+        The application uses advanced speech-to-text and text-to-speech pipelines integrated with OpenAI's GPT & Whisper models to deliver intelligent, conversational responses.
+        </p>
+      </>
+    ),
+    github: "https://github.com/mateopaez/voxa",
+    demo: "https://github.com/mateopaez/voxa",
+  },
+  2: {
+    title: "Gobber's Hoard",
+    image: projectTwo,
+    description: (
+      <>
+        <p>
+        Small looter/stealth video game developed in RPG Maker MV, built using JavaScript. Member of a 5-person team where my primary roles where
+        level design and building, implementing mechanics using custom made plugins, and general quality assurance and bug testing.
         </p>
       </>
     ),
     github: "https://Bathrobe-Studios.github.io",
     demo: "https://youtube.com/watch?v=H3yzrdtegsc",
-  },
-  2: {
-    title: "HabitUp",
-    image: projectTwo,
-    description: (
-      <>
-        <p>
-          Android personal wellness and social media application for aiding users in starting or maintaining consistent habit behaviour. Member of a 
-          6-person team where we developed in Android Studio IDE using Java and Firebase Realtime Database, and used Junit testing for quality assurance.
-        </p>
-      </>
-    ),
-    github: "https://github.com/CMPUT301F21T01/HabitUp",
-    demo: "https://github.com/CMPUT301F21T01/HabitUp",
   },
   3: {
     title: "Social Distribution",
